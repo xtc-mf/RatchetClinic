01ce8875ce6684b9c85a79f9e927565dd4d58787
Alexei <alexeirubers23@gmail.com>
2023-05-31 20:07:27 +0300

    create 3 tables Feedback, Employees, Review and 1 function to calculate average for rating in Review

--- Ratchet/models.py
+++ Ratchet/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db.models import Avg
+
 class Feedback(models.Model):
     feedback_name = models.CharField('Имя', max_length=50, default='')
     feedback_phone = models.CharField('Телефонный номер', max_length=20, null=False)
@@ -10,17 +13,18 @@ class Feedback(models.Model):
         verbose_name = 'Обратная связь'
         verbose_name_plural = 'Обратные связи'
 
-
 class Employees(models.Model):
     DEPATRAMENTS = [
-        ('Терапевт', 'Терапевт'),
-        ('Хирург', 'Хирург'),
-        ('Педиатр', 'Педиатр'),
-        ('Невролог', 'Невролог'),
-        ('Реабилитолог', 'Реабилитолог'),
-        ('Стоматолог', 'Стоматолог'),
-        ('Лор', 'Лор'),
-        ('Травмотолог', 'Травмотолог')
+        ('Терапевт', 'Терапевт'), #
+        ('Хирург', 'Хирург'), #
+        ('Педиатр', 'Педиатр'), #
+        ('Невролог', 'Невролог'), #
+        ('Реабилитолог', 'Реабилитолог'), #
+        ('Стоматолог', 'Стоматолог'), #
+        ('Лор', 'Лор'), #
+        ('Травмотолог', 'Травмотолог'), #
+        ('Психиатр', 'Психиатр'), #
+        ('Нарколог', 'Нарколог') #
     ]
     employee_full_name = models.CharField('Полное имя', max_length=200, default='Пример Пример Примеров')
     employee_age = models.IntegerField()
@@ -35,3 +39,21 @@ class Employees(models.Model):
     class Meta:
         verbose_name = 'Врач'
         verbose_name_plural = 'Врачи'
+
+class Review(models.Model):
+    review_name = models.CharField('Ваше имя', max_length=200, default='Гость')
+    reviwe_rate = models.IntegerField('Ваша оценка', validators=[
+            MinValueValidator(1),
+            MaxValueValidator(5)
+        ])
+    reviwe_text = models.TextField('Ваш отзыв')
+
+    def __str__(self):
+        return self.review_name + " оставил отзыв на оценку " + str(self.reviwe_rate) + " баллов"
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
+
+def calculate_average_rate():
+    average_rating = Review.objects.aggregate(Avg('reviwe_rate'))['reviwe_rate__avg']
+    return average_rating
