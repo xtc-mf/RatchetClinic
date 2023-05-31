6e84dbda77a6a83b1f6a657b19f2a71253589c81
Alexei <alexeirubers23@gmail.com>
2023-06-01 01:08:38 +0300

    register new model Client

--- Ratchet/admin.py
+++ Ratchet/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Feedback, Employees, Review
+from .models import Feedback, Employees, Review, Service, Client
 
 
 admin.site.register(Feedback)
 admin.site.register(Employees)
 admin.site.register(Review)
+admin.site.register(Service)
+admin.site.register(Client)
+
