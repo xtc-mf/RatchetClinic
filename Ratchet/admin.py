f7ef5375dfc46a5def6b6835a35ff751a0fbd4a8
Alexei <alexeirubers23@gmail.com>
2023-05-31 20:03:42 +0300

    register new tables

--- Ratchet/admin.py
+++ Ratchet/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Feedback, Employees
+from .models import Feedback, Employees, Review
 
 
 admin.site.register(Feedback)
 admin.site.register(Employees)
+admin.site.register(Review)
